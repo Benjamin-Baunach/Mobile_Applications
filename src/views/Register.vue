2ea59665cf43ce0db7211f990b6fb02e5bbc94c1
Benjamin Baunach <105782758+Benjamin-Baunach@users.noreply.github.com>
2024-04-29 15:31:44 +0200

    Bugfix on Register Page --> Scrolling problem fixed

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,7 +2,7 @@
  
     <AuthNavbar />
     
-    <div class="h-[calc(100vh_-_84px)] flex flex-col items-center ">
+    <div class="h-[calc(100vh_-_84px)] flex flex-col items-center overflow-y-auto">
       <form class="max-w-md flex flex-col container px-4 mx-auto gap-y-3" :class="{ 'opacity-50 blur-none' : success}" @submit="onSubmit">
       <div class="flex flex-row gap-x-3">
         <FormField v-slot="{ componentField }" name="fname">
